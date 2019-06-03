158c413c44e826cd9457e8d87bc5d61064c79238
chauncy <hl1782331304@gmail.com>
2019-06-03 11:31:14 +0800

    add file(店铺管理模块)

---
+++ distribution-parent/distribution-store/src/main/java/com/chauncy/store/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author huangwancheng
+ * @create 2019-06-03 11:25
+ */
+package com.chauncy.store;
---
+++ distribution-parent/distribution-store/src/main/java/com/chauncy/store/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author huangwancheng
+ * @create 2019-06-03 11:26
+ */
+package com.chauncy.store.service;
