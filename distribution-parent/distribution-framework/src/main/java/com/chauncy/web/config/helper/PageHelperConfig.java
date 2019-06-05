dd6cfab339f71a528a83d1d0645c69422795aef3
zhangrt <389454136@qq.com>
2019-06-05 10:04:47 +0800

    分页插件版本问题

--- distribution-parent/distribution-framework/src/main/java/com/chauncy/web/config/helper/PageHelperConfig.java
+++ distribution-parent/distribution-framework/src/main/java/com/chauncy/web/config/helper/PageHelperConfig.java
@@ -1,6 +1,7 @@
 package com.chauncy.web.config.helper;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +17,8 @@ public class PageHelperConfig {
      *     配置mybatis的分页插件pageHelper
      */
     @Bean
-    public PageHelper pageHelper(){
-        PageHelper pageHelper = new PageHelper();
+    public PageInterceptor pageHelper(){
+        PageInterceptor pageHelper = new PageInterceptor();
         Properties properties = new Properties();
         // 当该参数设置为 true 时，会将 RowBounds 中的 offset 参数当成 pageNum 使用，可以用页码和页面大小两个参数进行分页。
         properties.setProperty("offsetAsPageNum","true");
@@ -27,7 +28,7 @@ public class PageHelperConfig {
         //当该参数设置为 true 时，pageNum<=0 时会查询第一页， pageNum>pages（超过总数时），会查询最后一页
         properties.setProperty("reasonable","true");
         //配置mysql数据库的方言
-        properties.setProperty("dialect","mysql");
+        //properties.setProperty("dialect","mysql");
         pageHelper.setProperties(properties);
         return pageHelper;
     }
