1228ded6732b8656f4e40e81f6f8350382f2a80f
cheng <1782331304@qq.com>
2019-06-11 01:33:14 +0800

    update cros(尝试解决跨域问题)

--- distribution-parent/distribution-security/src/main/java/com/chauncy/security/config/WebSecurityConfig.java
+++ distribution-parent/distribution-security/src/main/java/com/chauncy/security/config/WebSecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.config.annotation.web.configurers.Expression
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * @Author huangwancheng
@@ -80,15 +81,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf().disable();
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = http
                 .authorizeRequests();
 
         //除配置文件忽略路径其它所有请求都需经过认证和授权
         for (String url : ignoredUrlsProperties.getUrls()) {
             registry.antMatchers(url).permitAll();
+
         }
 
-        registry.and()
+        registry.requestMatchers(CorsUtils::isPreFlightRequest).permitAll().and()
                 //表单登录方式
                 .formLogin()
                 .loginPage("/distribution/common/needLogin")
@@ -111,8 +114,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 //需要身份认证
                 .authenticated()
-                .and()
-                .cors()
                 .and()
                 //关闭跨站请求防护
                 .csrf().disable()
