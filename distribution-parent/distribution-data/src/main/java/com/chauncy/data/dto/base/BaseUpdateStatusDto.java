d48e5d0e489991e1bcfb053e4ca56ad639deebc2
xiaoye <849306235@qq.com>
2019-06-15 13:45:50 +0800

    店铺管理

--- distribution-parent/distribution-data/src/main/java/com/chauncy/data/dto/base/BaseUpdateStatusDto.java
+++ distribution-parent/distribution-data/src/main/java/com/chauncy/data/dto/base/BaseUpdateStatusDto.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @Author zhangrt
  * @Date 2019/6/13 23:39
@@ -12,8 +14,11 @@ import lombok.Data;
 @ApiModel(description = "启用或禁用")
 public class BaseUpdateStatusDto {
 
+
+    @NotNull(message = "id参数不能为空")
     private Long[] id;
 
     @ApiModelProperty("改变状态 1：启用  0：禁用")
+    @NotNull(message = "enabled参数不能为空")
     private Boolean enabled;
 }
