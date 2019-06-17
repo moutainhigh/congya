b600627ad15fce121f430b4bf76852ccbdca2bab
cheng <172331304@qq.com>
2019-06-17 23:17:34 +0800

    add file(商品基本信息之商品参数信息Dto)

---
+++ distribution-parent/distribution-data/src/main/java/com/chauncy/data/dto/supplier/good/add/AddGoodsParamValueDto.java
@@ -0,0 +1,24 @@
+package com.chauncy.data.dto.supplier.good.add;
+
+import com.chauncy.data.valid.annotation.NeedExistConstraint;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author cheng
+ * @create 2019-06-17 17:45
+ *
+ * 添加商品是添加商品对应的商品参数值
+ */
+@Data
+@ApiModel(description = "添加商品是添加商品对应的商品参数值")
+public class AddGoodsParamValueDto {
+
+    @ApiModelProperty(value = "商品参数ID")
+    @NeedExistConstraint(tableName = "pm_goods_attribute")
+    private Long id;
+
+    @ApiModelProperty(value = "商品参数值")
+    private String name;
+}
