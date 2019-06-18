05d0578b4f2f373a412e89461c47ba8cad77f188
xiaoye <849306235@qq.com>
2019-06-18 11:41:37 +0800

    店铺分类

--- distribution-parent/distribution-store/src/main/java/com/chauncy/store/category/service/impl/SmStoreCategoryServiceImpl.java
+++ distribution-parent/distribution-store/src/main/java/com/chauncy/store/category/service/impl/SmStoreCategoryServiceImpl.java
@@ -9,11 +9,13 @@ import com.chauncy.data.vo.JsonViewData;
 import com.chauncy.security.util.SecurityUtil;
 import com.chauncy.store.category.service.ISmStoreCategoryService;
 import com.chauncy.data.core.AbstractService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -106,5 +108,34 @@ public class SmStoreCategoryServiceImpl extends AbstractService<SmStoreCategoryM
         return new JsonViewData(ResultCode.SUCCESS, "查找成功", map);
     }
 
+
+    /**
+     * 条件查询
+     * @param storeCategorySearchDto
+     * @return
+     */
+    /*@Override
+    public PageInfo<SmStoreCategoryVo> searchPaging(StoreCategorySearchDto storeCategorySearchDto) {
+
+        Integer pageNo = storeCategorySearchDto.getPageNo()==null ? defaultPageNo : storeCategorySearchDto.getPageNo();
+        Integer pageSize = storeCategorySearchDto.getPageSize()==null ? defaultPageSize : storeCategorySearchDto.getPageSize();
+        String pageSoft = " create_time desc";
+
+        PageInfo<SmStoreCategoryVo> smStoreCategoryVoPageInfo = PageHelper.startPage(pageNo, pageSize, pageSoft)
+                .doSelectPageInfo(() -> smStoreCategoryMapper.searchPaging(storeCategorySearchDto));
+        return smStoreCategoryVoPageInfo;
+    }*/
+
+    /**
+     * 查询店铺所有标签
+     * @return
+     */
+    /*@Override
+    public JsonViewData searchAll() {
+
+        List<SmStoreCategoryVo> smStoreCategoryVoList = smStoreCategoryMapper.searchAll();
+        return new JsonViewData(ResultCode.SUCCESS, "查找成功", smStoreCategoryVoList);
+
+    }*/
 
 }
