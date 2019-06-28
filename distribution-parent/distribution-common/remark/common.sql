75d69af8cc88c809851ca584802a7969cf2f21ec
zhangrt <389454136@qq.cm>
2019-06-28 17:14:32 +0800

    树形工具类

--- distribution-parent/distribution-common/remark/common.sql
+++ distribution-parent/distribution-common/remark/common.sql
@@ -1,6 +1,5 @@
 
 -- 公用字段
-
 CREATE TABLE `aa` (
                     `id` bigint(20) NOT NULL COMMENT '运费模版ID',
                     `create_by` varchar(255) NOT NULL COMMENT '创建者',
@@ -10,3 +9,4 @@ CREATE TABLE `aa` (
                     `del_flag` tinyint(1) NOT NULL DEFAULT '0' COMMENT '删除标志 1-删除 0未删除',
                     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='运费模版说明表。平台运费模版+商家运费模版';
+
---
+++ distribution-parent/distribution-common/src/main/java/com/chauncy/common/util/third/SendSms.java
@@ -0,0 +1,39 @@
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+/*
+pom.xml
+<dependency>
+  <groupId>com.aliyun</groupId>
+  <artifactId>aliyun-java-sdk-core</artifactId>
+  <version>4.0.3</version>
+</dependency>
+*/
+public class SendSms {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("default", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("PhoneNumbers", "134");
+        request.putQueryParameter("SignName", "葱鸭百货");
+        request.putQueryParameter("TemplateCode", "短信模板id");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
