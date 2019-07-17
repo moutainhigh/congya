452aef9383a04a6a68f7fa1299b08a5b2a9698ea
wanchenghuang <1782331304@qq.com>
2019-07-17 13:24:07 +0800

    add file(排序字段枚举)

---
+++ distribution-parent/distribution-common/src/main/java/com/chauncy/common/enums/app/sort/SortFileEnum.java
@@ -0,0 +1,53 @@
+package com.chauncy.common.enums.app.sort;
+
+import com.chauncy.common.enums.BaseEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * @Author cheng
+ * @create 2019-07-17 13:14
+ * <p>
+ * 前端app排序字段
+ */
+@ApiModel("前端app排序字段")
+@Getter
+public enum SortFileEnum implements BaseEnum {
+
+    @ApiModelProperty("综合排序")
+    COMPREHENSIVE_SORT(1, ""),
+    @ApiModelProperty("销量排序")
+    SALES_SORT(2, ""),
+    @ApiModelProperty("价格排序")
+    PRICE_SORT(3, "");
+
+    private Integer id;
+    /**
+     * 手机验证码模板
+     */
+    private String name;
+
+    SortFileEnum(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //通过name获取结果
+    public static SortFileEnum getSortFileEnum(String name) {
+        for (SortFileEnum sortFileEnum : SortFileEnum.values()) {
+            if (sortFileEnum.name().equals(name))
+                return sortFileEnum;
+        }
+        return null;
+    }
+
+
+    @Override
+    public boolean isExist(Object field) {
+        return Objects.nonNull(getSortFileEnum(field.toString()));
+    }
+}
+
