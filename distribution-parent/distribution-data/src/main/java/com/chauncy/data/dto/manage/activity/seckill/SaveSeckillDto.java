451f1785d54009ea1e6709a26d2bc749e673ccf8
wanchenghuang <1782331304@qq.com>
2019-07-24 13:57:22 +0800

    update file(活动管理参数校验加强)

--- distribution-parent/distribution-data/src/main/java/com/chauncy/data/dto/manage/activity/seckill/SaveSeckillDto.java
+++ distribution-parent/distribution-data/src/main/java/com/chauncy/data/dto/manage/activity/seckill/SaveSeckillDto.java
@@ -29,38 +29,46 @@ public class SaveSeckillDto {
 
     @ApiModelProperty(value = "活动名称")
     @JSONField(ordinal = 1)
+    @NotNull(message = "活动名称不能为空")
     private String name;
 
     @ApiModelProperty(value = "排序数字")
     @JSONField(ordinal = 2)
+    @NotNull(message = "排序数字不能为空")
     private Integer sort;
 
     @ApiModelProperty(value = "会员ID")
     @JSONField(ordinal = 4)
+    @NotNull(message = "会员ID不能为空")
     private Long memberLevelId;
 
     @ApiModelProperty(value = "报名开始时间")
     @Future(message = "报名开始时间需要在当前时间之后")
     @JSONField(ordinal = 5)
+    @NotNull(message = "报名开始时间不能为空")
     private LocalDateTime registrationStartTime;
 
     @ApiModelProperty(value = "报名结束时间")
     @Future(message = "报名结束时间需要在当前时间之后")
     @JSONField(ordinal = 6)
+    @NotNull(message = "报名结束时间不能为空")
     private LocalDateTime registrationEndTime;
 
     @ApiModelProperty(value = "活动开始时间")
     @Future(message = "活动开始时间需要在当前时间之后")
     @JSONField(ordinal = 7)
+    @NotNull(message = "活动开始时间不能为空")
     private LocalDateTime activityStartTime;
 
     @ApiModelProperty(value = "活动结束时间")
     @Future(message = "活动结束时间需要在当前时间之后")
     @JSONField(ordinal = 8)
+    @NotNull(message = "活动结束时间不能为空")
     private LocalDateTime activityEndTime;
 
     @ApiModelProperty(value = "结束为0，默认为1启用")
     @JSONField(ordinal = 15)
+    @NotNull(message = "不能为空")
     private Boolean enable = true;
 
     @ApiModelProperty("绑定分类")
@@ -70,17 +78,21 @@ public class SaveSeckillDto {
 
     @ApiModelProperty(value = "活动说明(商家端查看)")
     @JSONField(ordinal = 14)
+    @NotNull(message = "活动说明不能为空")
     private String activityDescription;
 
     @ApiModelProperty(value = "活动简介(用户端查看)")
     @JSONField(ordinal = 13)
+    @NotNull(message = "活动简介不能为空")
     private String activityIntroduction;
 
     @ApiModelProperty(value = "活动标题")
     @JSONField(ordinal = 11)
+    @NotNull(message = "活动标题不能为空")
     private String activityTitle;
 
     @ApiModelProperty(value = "促销规则,积分抵扣比例 ")
+    @NotNull(message = "积分抵扣比例不能为空")
     private BigDecimal discountPriceRatio;
 
 }
