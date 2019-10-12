220753928ebc1fa5eb7fdd04687ee174231e1079
xiaoye <849306235@qq.com>
2019-10-12 15:12:05 +0800

    支付回调修改，方便测试

--- distribution-parent/distribution-web/src/main/java/com/chauncy/web/controller/pay/WxController.java
+++ distribution-parent/distribution-web/src/main/java/com/chauncy/web/controller/pay/WxController.java
@@ -99,10 +99,7 @@ public class WxController {
      *   支付异步结果通知，请求预支付订单时传入的地址
      *   官方文档 ：https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_7&index=3
      */
-    //@RequestMapping(value = "/wxPay/notify", method = {RequestMethod.GET, RequestMethod.POST}, produces={"application/xml;"})
-    //@RequestMapping(value = "/wxPay/notify", method = {RequestMethod.GET, RequestMethod.POST})
-    //@RequestMapping(value = "/wxPay/notify", method = {RequestMethod.GET, RequestMethod.POST})
-    @PostMapping("/wxPay/notify")
+    @RequestMapping(value = "/wxPay/notify", method = {RequestMethod.GET, RequestMethod.POST})
     @WebLog(description = "支付异步结果通知")
     public String wxPayNotify(HttpServletRequest request) {
         String resXml;
