f9ab9b88706550ee649a6a10025ff8c0e4a7fdd7
zhangrt <389454136@qq.cm>
2019-10-29 10:10:10 +0800

    第三方验证

--- distribution-parent/distribution-security/src/main/java/com/chauncy/security/exception/LoginFailLimitException.java
+++ distribution-parent/distribution-security/src/main/java/com/chauncy/security/exception/LoginFailLimitException.java
@@ -1,5 +1,6 @@
 package com.chauncy.security.exception;
 
+import com.chauncy.common.enums.system.ResultCode;
 import lombok.Data;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 
@@ -13,12 +14,19 @@ import org.springframework.security.authentication.InternalAuthenticationService
 @Data
 public class LoginFailLimitException extends InternalAuthenticationServiceException {
 
+    private ResultCode resultCode;
 
     private String msg;
 
     public LoginFailLimitException(String msg){
         super(msg);
         this.msg = msg;
+    }
+
+    public LoginFailLimitException(ResultCode resultCode){
+        super(resultCode.getDescription());
+        this.msg = resultCode.getDescription();
+        this.resultCode = resultCode;
     }
 
     public LoginFailLimitException(String msg, Throwable t) {
